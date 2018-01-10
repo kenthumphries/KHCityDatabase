c318e19c1905e9664822c346d953befdc80db1d1
Kent Humphries <kent.humphries@gmail.com>
2018-01-10 18:21:09 +0100

    Create bounding boxes to quickly identify which geographic area a city belongs to. This coarse grouping of cities allows for very fast lookup of lat/long to then perform reverse geocoding. Bounding boxes have a resolution of 0.5 degrees which translates to approximately 55km squared per box.

--- KHCityDatabaseCreator/CityDataImporter/CityRealmCreator.swift
+++ KHCityDatabaseCreator/CityDataImporter/CityRealmCreator.swift
@@ -96,6 +96,15 @@ class KHCityRealmCreator {
             }
         })
         
+        try realm.write {
+            let interactor = BoundingBoxInteractor()
+            for location in locations {
+                let identifier = interactor.locationIdentifier(for: location)
+                let boundingBox = interactor.boundingBox(forLocationIdentifier: identifier, in: realm)
+                boundingBox.contained.append(location)
+            }
+        }
+        
         return realm
     }
 
---
+++ KHCityDatabaseCreator/CityDataImporter/Models/BoundingBox.swift
@@ -0,0 +1,21 @@
+//
+//  BoundingBox.swift
+//  KHCityDatabaseCreator
+//
+//  Created by Kent Humphries on 10/1/18.
+//  Copyright © 2018 KentHumphries. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+open class BoundingBox: Object {
+    
+    @objc open dynamic var locationIdentifier : String = ""
+    let contained = List<City>()
+    
+    public convenience init(locationIdentifier: String) {
+        self.init()
+        self.locationIdentifier = locationIdentifier
+    }
+}
